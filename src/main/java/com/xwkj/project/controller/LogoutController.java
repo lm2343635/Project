56bc46ddfea53987f4fb90bafec146a4a118ae69
Meng Li <lm2343635@126.com>
2017-07-15 11:46:24 +0900

    Show project detail information for user.

--- src/main/java/com/xwkj/project/controller/LogoutController.java
+++ src/main/java/com/xwkj/project/controller/LogoutController.java
@@ -1,6 +1,7 @@
 package com.xwkj.project.controller;
 
 import com.xwkj.project.service.AdminManager;
+import com.xwkj.project.service.UserManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,12 @@ public class LogoutController {
     public void adminLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.getSession().removeAttribute(AdminManager.ADMIN_FLAG);
         response.sendRedirect("/admin");
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public void userLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().removeAttribute(UserManager.USER_FLAG);
+        response.sendRedirect("/user");
     }
 
 }
